f43f6a52f77074ca76a45ce32d41d3f857e2e257
mel-aguilar <44855284+mel-aguilar@users.noreply.github.com>
2019-10-16 15:57:27 -0700

    Update main.c

--- lab_chip/source/main.c
+++ lab_chip/source/main.c
@@ -14,10 +14,24 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRB = 0xFF;	PORTB = 0x00; //Configure port B's to 8 pins as outputs
+	DDRA = 0x00;	PORTA = 0xFF;	//configure port A as input, initialize to 11111111 
+	DDRB = 0xFF;	PORTB = 0x00; //Configure port B's to 8 pins as outputs, initialize to 00000000
+	
+	unsigned char led = 0x00;
+	unsigned char button = 0x00;
+	
+	
 	
     /* Insert your solution below */
     while (1) {
-	PORTB = 0x0F; //Writes port B's 8 pins with 00001111
+	//PORTB = 0x0F; //Writes port B's 8 pins with 00001111
+	   button ~= PINA & 0x01;
+	if (button) {
+		led = (led & 0xFC) | 0x01;
+	}
+	else {
+		led = (led & 0xFC) | 0x02;
+	}
+	PORTB = led;    
     }
 }
